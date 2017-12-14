70b7e5967fc5884e3f973a6335872d98a6f76fb3
Phil Snyder <subfivemile@gmail.com>
2017-12-14 10:45:29 -0800

    syntax error fix

--- annotator/Pipeline.py
+++ annotator/Pipeline.py
@@ -116,7 +116,7 @@ class Pipeline:
     def metaShape(self):
         """ Print shape of `self._meta` """
         if hasattr(self._meta, 'shape'):
-            print(self._meta.shape())
+            print(self._meta.shape)
         else:
             print("No metadata view set.")
 
