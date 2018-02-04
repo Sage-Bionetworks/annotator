da4964ebc09d0b578c461d0d04725a94d48a09ab
teslajoy <nasim@plenary.org>
2018-02-04 12:10:17 -0800

    updated setuppy with latest version and pytest requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
+import requests
+
+reqRelease = requests.get("https://api.github.com/repos/Sage-Bionetworks/annotator/releases")
+releaseVersion = reqRelease.json()[0]['tag_name']
+__version__=str(releaseVersion[1:len(releaseVersion)])
 
 setup(
     name='annotator',
-    version='0.1',
+    version=__version__,
     description='A Synapse annotations automation tool.',
     url='https://github.com/Sage-Bionetworks/annotator',
     author='Phil Snyder, Nasim Sanati',
@@ -14,4 +19,5 @@ setup(
              'bin/sync_manifest.py'],
     install_requires=[
         'pandas',
-        'synapseclient'])
+        'synapseclient'],
+    tests_require=['pytest'])
