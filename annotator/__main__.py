9565d3c78305ffeedefed8cffbd7cf0b8819220c
teslajoy <nasim@plenary.org>
2018-02-08 13:48:19 -0800

    reformat setup of commandline scripts in package

---
+++ annotator/__main__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import argparse
+import pandas
+import synapseclient
+from annotator import schema
+
+
+
