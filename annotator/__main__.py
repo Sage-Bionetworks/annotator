00e1e4e8b3393489905e0305818d49fe396873cc
teslajoy <nasim@plenary.org>
2018-02-08 13:52:55 -0800

    added synapse login function

--- annotator/__main__.py
+++ annotator/__main__.py
@@ -3,9 +3,28 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 import argparse
+import getpass
 import pandas
 import synapseclient
 from annotator import schema
 
+
+def synapseLogin():
+    """
+    First tries to login to synapse by finding the local auth key cached on user's computing platform, if not found,
+    prompts the user to provide their synapse user name and password, then caches the auth key on their computing
+    platform.
+
+    :return:
+    """
+    try:
+        syn = synapseclient.login()
+    except Exception as e:
+        print('Please provide your synapse username/email and password (You will only be prompted once)')
+        username = input("Username: ")
+        password = getpass.getpass(("Password for " + username + ": ").encode('utf-8'))
+        syn = synapseclient.login(email=username, password=password, rememberMe=True)
+
+    return syn
 
 
