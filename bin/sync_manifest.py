05306be9e75e729d54c0a2d3f93badfdf3d9f73f
teslajoy <nasim@plenary.org>
2018-02-09 11:01:44 -0800

    added more function documentations

--- bin/sync_manifest.py
+++ bin/sync_manifest.py
@@ -13,34 +13,48 @@ import synapseutils
 
 
 def _getLists(local_root, depth):
-    # walk through local directory
-    # get a list of dirs and a list of files
+    """
+    Given a depth, creates a list of directory and files hierarchy paths.
 
+    :param local_root:
+    :param depth:
+    :return:
+    """
     dir_list = []
     file_list = []
 
     for dirpath, _, filenames in os.walk(local_root):
+
         sub_dir = dirpath[len(local_root):]
         n = sub_dir.count(os.path.sep) + 1 if sub_dir != '' else 0
         dirpath = os.path.abspath(dirpath)
+
         if depth is not None:
             if n < depth:
                 dir_list.append(dirpath)
         else:
             dir_list.append(dirpath)
+
         for name in filenames:
             if not name.startswith('.'):
                 file_list.append(os.path.join(dirpath, name))
+
     return dir_list, file_list
 
 
 def _getSynapseDir(syn, synapse_id, local_root, dir_list):
-    # walk through Synapse
-    # update folders in Synapse to match the local dir
-    # get key-value pairs of dirname and synapse id
+    """
+    Walks through Synapse parent location hierarchy.
+    update folders in Synapse to match the local dir,
+    get key-value pairs of dirname and synapse id
 
+    :param syn:
+    :param synapse_id:
+    :param local_root:
+    :param dir_list:
+    :return:
+    """
     synapse_dir = {}
-
     synapse_root = syn.get(synapse_id)
 
     for (dirpath, dirpath_id), _, _ in synapseutils.walk(syn, synapse_id):
@@ -48,20 +62,29 @@ def _getSynapseDir(syn, synapse_id, local_root, dir_list):
         synapse_dir[dirpath] = dirpath_id
 
     for directory in dir_list:
+
         if not synapse_dir.has_key(directory):
             new_folder = synapseclient.Folder(os.path.basename(directory),
                                               synapse_dir[os.path.dirname(directory)])
             new_folder = syn.store(new_folder)
             synapse_dir[directory] = new_folder.id
+
     return synapse_dir
 
 
 def _getAnnotationKey(dirs):
-    # get a list of annotation keys
+    """
+     Get the list of annotation keys (manifest columns)
+
+    :param dirs:
+    :return:
+    """
     key_list = ['used', 'executed']
 
     if dirs is not None:
+
         for directory in dirs:
+
             if urlparse(directory).scheme != '':
                 jfile = urlopen(directory)
             else:
@@ -82,11 +105,22 @@ def _getAnnotationKey(dirs):
 
 
 def _getName(path, synapse_dir, local_root, depth):
+    """
+    Finds the name of files in local directory.
+
+    :param path:
+    :param synapse_dir:
+    :param local_root:
+    :param depth:
+    :return: name of file and it's associated parent location/benefactor
+    """
     path_no_root = path[len(os.path.abspath(local_root)):]
 
     if depth is not None and path_no_root.count(os.path.sep) > depth - 1:
+
         if str.startswith(path_no_root, '/'):
             path_no_root = path_no_root[1:]
+
         temp_name = path_no_root.split('/')[(depth - 1):]
         name = '_'.join(temp_name)
 
@@ -95,11 +129,22 @@ def _getName(path, synapse_dir, local_root, depth):
     else:
         name = os.path.basename(path)
         parent = synapse_dir[os.path.dirname(path)]
+        
     return name, parent
 
 
 def create(file_list, key_list, synapse_dir, local_root, depth, tab):
-    # create manifest
+    """
+    Creates manifest designed for the input of sync function.
+
+    :param file_list:
+    :param key_list:
+    :param synapse_dir:
+    :param local_root:
+    :param depth:
+    :param tab:
+    :return:
+    """
 
     result = pandas.DataFrame()
     result['path'] = file_list
