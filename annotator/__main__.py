a8d27deebc47ea47881b4c33ea60a32e06b7b281
teslajoy <nasim@plenary.org>
2018-02-09 13:13:24 -0800

    moved sync manifest generator functions to main

--- annotator/__main__.py
+++ annotator/__main__.py
@@ -5,16 +5,17 @@ from __future__ import unicode_literals
 from future.utils import iteritems
 from six.moves.urllib.parse import urlparse
 from six.moves.urllib.request import urlopen
+import subprocess
 import os
 import sys
 import json
-import urllib
 import six
 import argparse
 import getpass
 import pandas
+import synapseutils
 import synapseclient
-from synapseclient import Entity, Project, Column, Team, Wiki
+from synapseclient import Entity, Project, Column, Team, Wiki, Folder
 from annotator import schema
 
 
@@ -169,6 +170,178 @@ def emptyView(args, syn):
     print(view)
 
 
+def _getLists(local_root, depth):
+    """
+    Given a depth, creates a list of directory and files hierarchy paths.
+
+    :param local_root:
+    :param depth:
+    :return:
+    """
+    dir_list = []
+    file_list = []
+
+    for dirpath, _, filenames in os.walk(local_root):
+
+        sub_dir = dirpath[len(local_root):]
+        n = sub_dir.count(os.path.sep) + 1 if sub_dir != '' else 0
+        dirpath = os.path.abspath(dirpath)
+
+        if depth is not None:
+            if n < depth:
+                dir_list.append(dirpath)
+        else:
+            dir_list.append(dirpath)
+
+        for name in filenames:
+            if not name.startswith('.'):
+                file_list.append(os.path.join(dirpath, name))
+
+    return dir_list, file_list
+
+
+def _getSynapseDir(syn, synapse_id, local_root, dir_list):
+    """
+    1. Walks through Synapse parent location hierarchy.
+    2. update folders in Synapse to match the local dir,
+    3. get key-value pairs of dirname and synapse id
+
+    :param syn:
+    :param synapse_id:
+    :param local_root:
+    :param dir_list:
+    :return:
+    """
+    synapse_dir = {}
+    synapse_root = syn.get(synapse_id)
+
+    for (dirpath, dirpath_id), _, _ in synapseutils.walk(syn, synapse_id):
+        dirpath = dirpath.replace(synapse_root.name, os.path.abspath(local_root))
+        synapse_dir[dirpath] = dirpath_id
+
+    for directory in dir_list:
+
+        if not synapse_dir.has_key(directory):
+            new_folder = synapseclient.Folder(os.path.basename(directory),
+                                              synapse_dir[os.path.dirname(directory)])
+            new_folder = syn.store(new_folder)
+            synapse_dir[directory] = new_folder.id
+
+    return synapse_dir
+
+
+def _getAnnotationKey(dirs):
+    """
+     Get the list of annotation keys (manifest columns)
+
+    :param dirs:
+    :return:
+    """
+    key_list = ['used', 'executed']
+
+    if dirs is not None:
+
+        for directory in dirs:
+
+            if urlparse(directory).scheme != '':
+                jfile = urlopen(directory)
+            else:
+                jfile = open(directory, 'r')
+
+            base, ext = os.path.splitext(os.path.basename(directory))
+
+            if ext == '.json':
+                data = json.load(jfile)
+            else:
+                sys.stderr.write('File %s cannot be parsed. JSON format is required.\n' % directory)
+
+            data = pandas.DataFrame(data)
+            annotation_key = data['name']
+            key_list = key_list + list(annotation_key)
+
+    return key_list
+
+
+def _getName(path, synapse_dir, local_root, depth):
+    """
+    Finds the name of files in local directory.
+
+    :param path:
+    :param synapse_dir:
+    :param local_root:
+    :param depth:
+    :return: name of file and it's associated parent location/benefactor
+    """
+    path_no_root = path[len(os.path.abspath(local_root)):]
+
+    if depth is not None and path_no_root.count(os.path.sep) > depth - 1:
+
+        if str.startswith(path_no_root, '/'):
+            path_no_root = path_no_root[1:]
+
+        temp_name = path_no_root.split('/')[(depth - 1):]
+        name = '_'.join(temp_name)
+
+        temp_name = '/'.join(temp_name)
+        parent = synapse_dir[os.path.dirname(path[:-len(temp_name)])]
+    else:
+        name = os.path.basename(path)
+        parent = synapse_dir[os.path.dirname(path)]
+
+    return name, parent
+
+
+def create_sync_manifest(file_list, key_list, synapse_dir, local_root, depth):
+    """
+    Creates manifest designed for the input of sync function.
+
+    :param file_list:
+    :param key_list:
+    :param synapse_dir:
+    :param local_root:
+    :param depth:
+    :return:
+    """
+    result = pandas.DataFrame()
+    result['path'] = file_list
+    result['name'] = ""
+    result['parent'] = ""
+
+    for index, row in result.iterrows():
+        row[['name', 'parent']] = _getName(row['path'], synapse_dir, local_root, depth)
+
+    cols = list(result.columns)
+    result = pandas.concat([result, pandas.DataFrame(columns=key_list)])
+    result = result[cols + key_list] # reorder the columns
+    save_path = os.path.join(os.getcwd(),'annotations_manifest.csv')
+    result.to_csv(save_path, index=False)
+    sys.stderr.write('Manifest has been created on local directory: \n %s \n' % os.getcwd())
+
+
+def sync_manifest(args, syn):
+    """
+    Creates a manifest (filepath by annotations) designed for the input of synapse sync
+    function to facilitate file organization and annotations of those files on synapse.
+    :param args:
+    :param syn:
+    :return:
+    """
+    sys.stderr.write('Preparing to create manifest\n')
+    local_root = args.directory
+    synapse_id = args.id
+    annotations = args.files
+    depth = args.n
+
+    if depth is not None:
+        depth = int(depth)
+
+    dir_list, file_list = _getLists(local_root, depth)
+    synapse_dir = _getSynapseDir(syn, synapse_id, local_root, dir_list)
+    key_list = _getAnnotationKey(annotations)
+
+    create_sync_manifest(file_list, key_list, synapse_dir, local_root, depth)
+
+
 def buildParser():
     """
 
@@ -224,8 +397,6 @@ def buildParser():
                                                        'folders to mirror. Any file/folder beyond this number would '
                                                        'be expanded into the hierarchy number indicated.',
                                      default=None, required=False)
-    parser_syncmanifest.add_argument('--tab', action='store_true', help='tab delaminated manifest will be echoed into '
-                                                                        'standard output for piping', required=False)
     parser_syncmanifest.set_defaults(func=sync_manifest)
 
     return parser
@@ -263,7 +434,6 @@ def performMain(args, syn):
 
 def main():
     args = buildParser().parse_args()
-    print(args)
     syn = synapseLogin()
 
     performMain(args, syn)
